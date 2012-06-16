4243497f8b104d913f5968f7b1d214d3d2e8b4e4
Sean <dearseanharvey@gmail.com>
2012-06-15 17:03:24 -0700

    Init Commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -6,8 +5,9 @@
 var express = require('express')
   , routes = require('./routes');
 
-var app = module.exports = express.createServer()
-  , io = require('socket.io').listen(app);
+var app = module.exports = express.createServer();
+  
+var mysql = require('mysql');
 
 // Configuration
 app.configure(function(){
@@ -16,7 +16,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ secret: 'secret'}));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -32,52 +32,125 @@ app.configure('production', function(){
 // Routes
 app.get('/', routes.index);
 
-// Init users & chats arrays
-var users = [];
-var chats = [];
+// Triggers when a user loads up the page
+/* io.sockets.on("connection", function(socket){ */
+var loginTime = new Date();
 
-// Accepts an array & the value to be removed.
-var removeUser = function(array, value){
-	// Loops through each index of the array
-	for(var i in array){
-		// If it finds a match remove it from the array and return it
-		if(array[i] == value){
-			array.splice(i,1);
-			return array;
-		};
-	};
-};
+// Set up DB connection
+var connection = mysql.createConnection({
+	host     : 'localhost'
+  , database : 'borrow'
+  , user     : 'borrow'
+  , password : '******'
 
-io.sockets.on("connection", function(socket){
-	
-	// When a user submits their username we push
-	// them to the array which stores all current users
-	socket.on("login", function(user){
-		users.push(user);
-		console.log(users);
-		
-		// Send updated users array to all sockets
-		io.sockets.emit("updateUsers", users);
-		
-	});
-	
-	socket.on("newChat", function(chat){
-		console.log(chat);
-		io.sockets.emit("updateChat", chat);
-	});
-	
-	socket.on("logout", function(user){
-		if(users){
-			users = removeUser(users, user);
-			//console.log(users);
-			//io.sockets.broadcast.emit(user + 'has left');
-			io.sockets.emit("updateUsers", users);
-			socket.disconnect();
+});
+
+// Create connection with DB
+connection.connect();
+
+app.post('/login', function(req, res){
+	var sql = 'SELECT * FROM users WHERE user = "'+req.body['user']+'" && pass = "'+req.body['pass']+'"';
+	connection.query(sql, function(err, rows){
+		if(err){
+			throw err;
 		} else {
-			console.log(users + "no users");
+			if(rows.length > 0){
+				res.json({status: 'ok'});	
+			} else {
+				res.json({status: 'We couldnt find that user/pass combination in our records.'});
+			};	
 		};
 	});
+});
 
+// Get Current	
+app.get('/getCurrentItems', function(req, res){
+	// Save the sql query to a var
+	var sql = 'SELECT * FROM items WHERE returned = 0 ORDER BY date DESC';
+	// Send query to the DB
+	connection.query(sql, function(err, rows, fields){
+		// Check for errors
+		if(err){
+			throw err;
+		};
+		var result = '';
+		// Loop through each row and build markup around each row field
+		for(i in rows) {
+			result += '<tr><input type="hidden" name="id" value="'+rows[i].id+'" /><td class="date">'+rows[i].date+'</td><td class="item">'+rows[i].item+'</td><td class="lent_to">'+rows[i].lent_to+'</td><td class="return_date">'+rows[i].date_of_return+'</td><td class="btns"><a class="save" href="#"></a><a class="edit" href="#"></a></td></tr>';
+		};
+		// Pass the result as an object to the client
+		res.json({result: result});
+	});
+});
+
+// Past
+app.get('/getPastItems', function(req, res){
+	// Save the sql query to a var
+	var sql = 'SELECT * FROM items WHERE returned = 1 ORDER BY date DESC';
+	// Send query to the DB
+	connection.query(sql, function(err, rows, fields){
+		// Check for errors
+		if(err){
+			throw err;
+		};
+		var result = '';
+		// Loop through each row and build markup around each row field
+		for(i in rows) {
+			result += '<tr><input type="hidden" name="id" value="'+rows[i].id+'" /><td class="date">'+rows[i].date+'</td><td class="item">'+rows[i].item+'</td><td class="lent_to">'+rows[i].lent_to+'</td><td class="return_date">'+rows[i].date_of_return+'</td><td class="btns"><a class="trash" href="javascript:void(0);"></a></td></tr>';
+		};
+		// Pass the response as an object to the client
+		res.json({result: result});
+	});
+});
+
+// Add
+app.post('/addItem', function(req, res){
+	// Save the sql query to a var
+	var sql = 'INSERT INTO items (date, item, lent_to, date_of_return) VALUES ("'+req.body['date']+'", "'+req.body['item']+'", "'+req.body['lent_to']+'", "'+req.body['data_of_return']+'")';
+	
+	connection.query(sql, function(err, rows, fields){
+		if(err){
+			throw err;
+		} else {
+			res.json({status: 'ok'});
+		};
+	});
+});
+
+// Update
+app.post('/updateItems', function(req, res){
+	// If the updated item is marked as returned, we need to the add that status
+	// and the date of return to the DB and 
+	if(req.body['returned'] == 'true'){
+		// Set returned to be 1 or True
+		var returned = 1;
+		// Set up the query to update the item as returned
+		var sql = 'UPDATE items SET date = "'+req.body['date']+'", item = "'+req.body['item']+'", lent_to = "'+req.body['lent_to']+'", date_of_return = "'+req.body['return_date']+'", returned = "'+returned+'", date_returned = "'+req.body['date_returned']+'" WHERE id = "'+req.body['id']+'"';
+	} else {
+		// If the item wasn't marked as returned, set returned to 0 or False
+		var returned = 0;
+		// Set up the query to just update the item, not mark it as returned
+		var sql = 'UPDATE items SET date = "'+req.body['date']+'", item = "'+req.body['item']+'", lent_to = "'+req.body['lent_to']+'", date_of_return = "'+req.body['return_date']+'", returned = "'+returned+'" WHERE id = "'+req.body['id']+'"';
+	};
+	// 
+	connection.query(sql, function(err, rows, fields){
+		if(err){
+			throw err;
+		} else {
+			res.json({status: 'ok'});
+		};
+	});
+});
+
+app.post('/deleteItem', function(req, res){
+	var sql = 'DELETE FROM items WHERE id = "'+req.body['id']+'"';
+	connection.query(sql, function(err, rows, fields){
+		if(err){
+			throw err;
+		} else {
+			res.json({status: 'ok'});
+		};
+	});
 });
 
 var port = process.env.PORT || 3000;
@@ -85,7 +158,6 @@ var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
-
 
 callback = function(err, result) {};
 // var async = require('async')	
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,76 +1,251 @@
 $(function(){
+	// Toggles Current and Returned item list views	
+	$("aside ul li").click(function(){
+		if($(this).hasClass("active")){
+			return false;
+		} else {
+			$(this).addClass("active");
+			$(this).siblings("li").removeClass("active");
+			if($(this).attr("id") == "past"){
+				$("table.current, .current-title").hide();
+				$("table.past, .past-title").fadeIn(300);
+			} else {
+				$("table.past, .past-title").hide();
+				$("table.current, .current-title").fadeIn(300);
+			};
+		};
+	});
 	
-	var chatApp = function(){
-		
-		var socket = io.connect("http://localhost:3000");
-		
-		socket.on("connect", function(){
-			$("#login").submit(function(event){
+	$("table, a.logout, h2").hide();
+	
+	
+	// Main app 
+	var Borrow = function(){
+		function login(){
+			$("form#login").submit(function(event){
 				event.preventDefault();
-				// Save username entered by user
-				var user = $("input[name=user]").val();
-				// Hide login
-				$("#login").hide();
-				// Display greeting & logout
-				$(".greeting").html("Hello, <strong>" +user+ "</strong>").show();
-				$(".logout").show();
-				// Emit login event and pass username off to server
-				socket.emit("login", user);
-			});
-		});
-		
-		// On the serverside 'updateUsers' event call an anom. function and pass in the
-		// users array which store online users on the server.
-		socket.on("updateUsers", function(users){
-			console.log(users);
-			// Clear out current online user list
-			$("#online > ul").empty();
-			// Loop through users array then wrap each online user in an li
-			$.each(users, function(i){
-				$("<li>"+ users[i] +"</li>").appendTo("#online > ul");
-			});
-		});
-		
-		// On the server
-		socket.on("updateChat", function(chat){
-			console.log(chat);
-			$("<tr><td class='user'>"+ chat.user +":"+"</td><td class='message'>"+ chat.message +"</td></tr>").appendTo("#messages > table");
-		});
-		
-		
-		var logout = function(){
-			$(".logout").click(function(){	
-				$(this).hide();
-				$(".greeting").hide();
-				$("#login").show();
-				var user = $("p.greeting > strong").text();
-				$("p.greeting, #online > ul").detach();
-				$("#messages > table").detach();
-				socket.emit("logout", user);
+				$.post("/login", {
+					user : $("input[name=user]").val(),
+					pass : $("input[name=password]").val()
+				}, function(data){
+					data = data.status;
+					if(data.trim() == 'ok'){
+						$("table.current, a.logout, h2.current-title").fadeIn(300);
+						$("#login").fadeOut(300);
+						$("header p").text("Welcome, " + $("input[name=user]").val()).fadeIn(300);
+					} else {
+						alert(data.status);
+					};
+				});
+				return false;
 			});
 		};
 		
-		var addChat = function(){
-			$("#chats").submit(function(event){
-				if($("p.greeting").text()){
-					event.preventDefault();
-					var user = $(".greeting > strong").text();
-					var message = $("input[name=chats]").val();
-					$("input[name=chats]").val("");
-					var chat = {
-						user: user,
-						message: message
-					};
-					// Emit the newChat event and pass off the current chat
-					socket.emit("newChat", chat);
-				} else {
-					alert("You must pick a nickname and login before sending anything.");
-					return false;
+		function logout(){
+			$("a.logout").click(function(){
+				$("table, header p, a.logout, h2").fadeOut(300);
+				$("#login").fadeIn(300);
+			});
+		};
+	
+		function checkHeight(){
+			// Get main content height
+			var mainHeight = $("article").height();
+			
+			// Check to see if sidebar height is less than
+			// the content area's height if it is make sidebar
+			// height the same as the content area
+			if($("article aside").height() < mainHeight){
+				$("article aside").height(mainHeight + 20);
+			};
+		};
+		
+		function getCurrentItems(){
+			$.get("/getCurrentItems", function(data){
+				if(data){
+					$("table.current tbody").empty();
+					$(data.result).appendTo("table.current tbody");
+					// Let tablesorter know content was changed
+					$("table").trigger("update");
 				};
 			});
-		};	
-		addChat();
-		logout();	
+		};
+		
+		function getPastItems(){
+			$.get("/getPastItems", function(data){
+				if(data){
+					$("table.past tbody").empty();
+					$(data.result).appendTo("table.past tbody");
+					// Let tablesorter know content was changed
+					$("table").trigger("update");
+				};
+			});
+		};
+		
+		function addItem(){
+			var fields = "<td class='date'><input type='text' name='date' ></td><td class='item'><input type='text' name='item' ></td><td class='lent_to'><input type='text' name='lent_to' ></td><td class='return_date'><input type='text' name='return_date' ></td>";
+			// Wait of button to be clicked
+			$(".add-to-list").click(function(){
+				// Make sure user is adding to the current list
+				if($("aside ul li#past").hasClass("active")){
+					alert("You can only add items to the current list");
+					return false;
+				};
+				$("<tr>"+ fields +"<td class='btns'><a class='save' style='display: inline-block;' href='javascript:void(0)'></a><a class='edit' href='javascript:void(0)'></a></td></tr>").appendTo("table.current tbody").hide().fadeIn(300);
+				
+				$("a.save").click(function(){
+					// Grab all td's within the current tr
+					var tds = $(this).parent().siblings();
+					// Grab all input's within td's
+					var inputs = $(this).parent().siblings().children();
+					// Loop thru each td and replace it's html with the
+					// current value of each input inside of it
+					$(tds).hide();
+					$(tds).each(function(i){
+						$(this).html($(inputs[i]).val());
+					});
+					$(tds).fadeIn(300);
+					// Hide the save button
+					$(this).hide();
+					// Post updated values to php so they can
+					// be updated in the db
+					console.log($(this).parent().siblings());
+					$.post("/addItem", {
+						date        : $(this).parent().siblings("td.date").text(),
+						item        : $(this).parent().siblings("td.item").text(),
+						lent_to     : $(this).parent().siblings("td.lent_to").text(),
+						return_date : $(this).parent().siblings("td.return_date").text()
+					}, function(data){
+						// Clean up data str
+						data = data.status.toLowerCase().trim();
+						// Make sure we get an 'ok' response from the server
+						if(data == 'ok'){
+							// Pull new data from db
+							getCurrentItems();
+							checkHeight();
+							$("table.current").tablesorter();
+							// Let the user know things are good
+							$("<p class='notify'>").text("Item successfully saved!").prependTo("header").hide().show().animate({opacity: 1.0}, 3000).fadeOut(1000);
+						} else {
+							// If things aren't ok, "alert" the server error
+							alert("Fail!: " + data);
+						}
+					});
+				});
+			});
+		};
+		
+		function editItem(){
+			// Bind the click event to the body as it's an unchanged element during 
+			// the ajax calls in the DOM, then check to see if the target is an edit link
+			$("body").click(function(e){
+				if($(e.target).is("a.edit")){
+					var self = $(e.target);
+					// Grab the save link for the currently selected row and display it
+					var save = $(self).siblings("a.save");
+					$(self).hide();
+					$(save).css("display", "inline-block");
+					var siblings = $(self).parent().siblings("td");
+					var fields = [
+						"<input type='text' name='date' />",
+						"<input type='text' name='item' />",
+						"<input type='text' name='lent_to' />",
+						"<input type='text' name='return_date' />"
+					];
+					$(siblings).hide();
+					$(siblings).each(function(i){
+						$(this).html($(fields[i]).attr("value", $(this).text()));
+					});
+					$(siblings).fadeIn(300);
+					//console.log();
+					
+					$(siblings[siblings.length - 1]).append("&nbsp;<input type='checkbox' name='returned' />Returned?");
+					
+					$(save).click(function(){
+						var returned = $("input[name=returned]").is(":checked");
+						// Grab all td's within the current tr
+						var tds = $(this).parent().siblings("td");
+						// Grab all input's within td's
+						var inputs = $(this).parent().siblings("td").children();
+						$(tds).hide();
+						// Loop thru each td and replace it's html with the
+						// current value of each input inside of it
+						$(tds).each(function(i){
+							$(this).html($(inputs[i]).val());
+						});
+						$(tds).fadeIn(300);
+						// Hide the save button
+						$(this).hide();
+						// Show edit button
+						$(self).css("display", "inline-block");
+						// Post updated values to php so they can
+						// be updated in the db
+						$.post("/updateItems", {
+							id          : $(this).parent().siblings("input[name=id]").val(),
+							date        : $(this).parent().siblings("td.date").text(),
+							item        : $(this).parent().siblings("td.item").text(),
+							lent_to     : $(this).parent().siblings("td.lent_to").text(),
+							return_date : $(this).parent().siblings("td.return_date").text(),
+							returned    : returned
+						}, function(data){
+							data = data.status;
+							if(data.trim() == 'ok'){
+								$("<p class='notify'>").text("Changes successfully saved!").prependTo("header").hide().show().animate({opacity: 1.0}, 3000).fadeOut(1000);
+								//console.log(data);
+								getCurrentItems();
+								getPastItems();
+								checkHeight();
+								$("table.current").tablesorter();
+							} else {
+								alert("Fail: " + data);
+							};
+						});
+					});
+				 };
+			});
+		};
+		
+		function deleteItem(){
+			$("body").click(function(e){
+				if($(e.target).is("a.trash")){
+					var self = $(e.target);
+					var siblings = $(self).parent().siblings();
+					var conf = confirm("You sure you want to delete this item?");
+					if(conf === true){
+						$(siblings).parent("tr").hide(300).remove();
+						// Get current date
+						var date_returned = new Date();
+						// Format date
+						date_returned = date_returned.format("m/dd/yy"); 
+						$.post("/deleteItem", {
+							id            : $(siblings[0]).val(),
+							date_returned : date_returned
+						}, function(data){
+							data = data.status;
+							if(data.trim() == "ok"){
+								$("<p class='notify'>").text("Item deleted successfully!")
+								.prependTo("header")
+								.hide().show().animate({opacity: 1.0}, 3000).fadeOut(1000);
+							} else {
+								alert("Fail: " + data);
+							}
+						});
+					};
+				};
+			});
+		};
+		
+		checkHeight();
+		login();
+		logout();
+		addItem();
+		editItem();
+		getCurrentItems();
+		getPastItems();
+		deleteItem();
+		
 	};
-	chatApp();
+	Borrow();
+	$("table.current").tablesorter();
+	$("table.past").tablesorter();
 });
